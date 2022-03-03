43237a64d8759cc7dd31b2e4580ceb003bc5c173
Eva Fong <eva.fong@nhrec.org>
2022-03-03 03:54:22 -0500

    added the new json file to character view

--- src/components/buttons-images.vue
+++ src/components/buttons-images.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flexCon">
-    <div class="image" v-for="boxes in imagesButtons" v-bind:key="boxes.url">
-      <img v-bind:src="boxes.url">
+  <div class="flexImages">
+    <div v-for="box in boxes" v-bind:key="box.id">
+      <img class="image" :style="{'border-color': box.color}" :src='box.img' />
     </div>
-    <div class="btn" v-for="boxes in imagesButtons" v-bind:key="boxes.button">
-      <button>{{ boxes.button }}</button>
+  </div>
+  <div class="flexBtns">
+    <div v-for="box in boxes" v-bind:key="box.id">
+      <button class="btn" :style="{'background-color': box.color}">{{ box.button }}</button>
     </div>
   </div>
 </template>
@@ -14,26 +16,36 @@ import imagesButtons from "../json/images.json";
 export default {
   data() {
     return {
-      
+      boxes: imagesButtons.boxes,
+      clr: imagesButtons.boxes.color
     };
   },
 };
 </script>
 
 <style scoped>
-.flexCon{
+.flexImages,
+.flexBtns {
   display: flex;
   justify-content: space-around;
+  align-items: center;
+  margin-right: 200px;
+  margin-left: 200px;
 }
 .image {
   border: 10px solid;
+  border-radius: 20%;
+  flex-basis: 30%;
+  margin-top: 100px;
+  margin-bottom: 10px;
 }
 .btn {
+  background-color: var(clr);
   font-family: monospace;
   font-size: 25px;
   text-align: center;
   border-radius: 20%;
+  width: 150px;
+  height: 80px;
 }
-
-
 </style>
--- src/views/characters.vue
+++ src/views/characters.vue
@@ -1,68 +1,41 @@
 <template>
-  <div id="characters">
-    <button @click="router.push('/')" id="homeButton"><router-link to="/">Home</router-link></button>
-    <div v-show="$route.name == 'tanjiro'">
-      <h1 class="title">About Tanjiro</h1>
-      <img src="../assets/tanjiro.jpeg" id="tan" class="images" />
-      <ul class="info">
-        <li v-for="facts in tanFacts" v-bind:key="facts.text">
-          {{ facts.text }}
-        </li>
-      </ul>
-      <p class="qt">
-        "{{ quotes.tanjiro }}"
-      </p>
-    </div>
-    <div v-show="$route.name == 'zenitsu'">
-      <h1 class="title">About Zenitsu</h1>
-      <img src="../assets/zenitsu.jpeg" class="images" id="zen" />
-      <ul class="info">
-        <li v-for="facts in zenFacts" v-bind:key="facts.text">
-          {{ facts.text }}
-        </li>
-      </ul>
-      <p class="qt">
-        "{{ quotes.zenitsu }}"
-      </p>
-    </div>
-    <div v-show="$route.name == 'inosuke'">
-      <h1 class="title">About Inosuke</h1>
-      <img src="../assets/inosuke.jpeg" class="images" id="ino" />
-      <ul class="info">
-        <li v-for="facts in inoFacts" v-bind:key="facts.text">
-          {{ facts.text }}
-        </li>
-      </ul>
-      <p class="qt">
-       "{{ quotes.inosuke }}"
-      </p>
+  <div>
+    <button @click="router.push('/')" id="homeButton">
+      <router-link to="/">Home</router-link>
+    </button>
+    <div class="container">
+      <div v-for="character in characters" v-bind:key="character.id">
+        <span class="title">
+          <h>About {{ character.name }}</h>
+        </span>
+        <span class="list">
+          <ul>
+            <li v-for="text in character.desc" v-bind:key="text">
+              {{ text }}
+            </li>
+          </ul>
+        </span>
+        <div class="qt">
+          <p>{{ character.quote }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import factsJson from "../json/facts.json";
+import facts from "../json/facts.json";
 export default {
   data() {
     return {
-      tanFacts: factsJson.tanjiro,
-      zenFacts: factsJson.zenitsu,
-      inoFacts: factsJson.inosuke,
-      quotes: factsJson.quotes,
+      characters: facts.character,
+      descript: facts.character.desc,
     };
   },
 };
 </script>
-
+  </div>
 <style scoped>
-.qt {
-  position: absolute;
-  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-  width: 600px;
-  font-size: 25px;
-  left: 740px;
-  top: 335px;
-}
 #homeButton {
   position: absolute;
   right: 20px;
@@ -73,36 +46,13 @@ export default {
   font-family: monospace;
   background-color: wheat;
 }
-.info {
-  position: absolute;
-  font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
-  width: 600px;
-  font-size: 25px;
-  left: 740px;
-}
-
-#tan {
-  border-color: crimson;
-}
-#zen {
-  border-color: yellow;
-}
-#ino {
-  border-color: cornflowerblue;
-}
-.images {
-  position: absolute;
-  width: 300px;
-  left: 350px;
-  height: 375px;
-  border-radius: 10%;
-  border: 10px solid;
+.container {
+  display: flex;
+  font-family: monospace;
+  gap: 20px;
 }
 .title {
-  position: relative;
-  text-align: center;
-  font-family: monospace;
-  font-size: 70px;
-  font: bold;
+  font-size: 60px;
 }
+
 </style>
