3d8f78e7cfc6876ce58ff45fca84fa93a7dd108c
Eva Fong <eva.fong@nhrec.org>
2022-03-08 21:29:20 -0500

    pics working for home

--- src/components/buttons-images.vue
+++ src/components/buttons-images.vue
@@ -1,24 +1,50 @@
 <template>
-  <div class="flexImages">
-    <div v-for="box in boxes" v-bind:key="box.id">
-      <img class="image" :style="{'border-color': box.color}" :src='box.img' />
+  <div>
+    <div class="flexImages">
+      <div v-for="character in characters" v-bind:key="character.name">
+        <img
+          :src="getImgUrl(character.name.toLowerCase())"
+          class="image"
+          :style="{ 'border-color': character.color }"
+        />
+        <button
+          @click="$router.push(character.name.toLowerCase())"
+          class="btn"
+          :style="{ 'background-color': character.color }"
+        >
+          {{ character.name }}
+        </button>
+      </div>
     </div>
-  </div>
-  <div class="flexBtns">
-    <div v-for="box in boxes" v-bind:key="box.id">
-      <button class="btn" :style="{'background-color': box.color}">{{ box.button }}</button>
-    </div>
+    <!--    <div class="flexBtns">
+      <div v-for="character in characters" v-bind:key="character.name">
+        <button
+          @click="$router.push(character.name.toLowerCase())"
+          class="btn"
+          :style="{ 'background-color': character.color }"
+        >
+          {{ character.name }}
+        </button>
+      </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-import imagesButtons from "../json/images.json";
+import facts from "../json/facts.json";
+const tanjiro = require("../assets/tanjiro.jpeg");
+const zenitsu = require("../assets/zenitsu.jpeg");
+const inosuke = require("../assets/inosuke.jpeg");
 export default {
   data() {
     return {
-      boxes: imagesButtons.boxes,
-      clr: imagesButtons.boxes.color
+      characters: facts.character,
     };
+  },
+  methods: {
+    getImgUrl(pic) {
+      return { tanjiro, zenitsu, inosuke }[pic];
+    },
   },
 };
 </script>
@@ -35,12 +61,10 @@ export default {
 .image {
   border: 10px solid;
   border-radius: 20%;
-  flex-basis: 30%;
   margin-top: 100px;
   margin-bottom: 10px;
 }
 .btn {
-  background-color: var(clr);
   font-family: monospace;
   font-size: 25px;
   text-align: center;
